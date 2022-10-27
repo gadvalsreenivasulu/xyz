7ae1bf32d9052aa92d0eacb9413d1741b4ed6eeb
sgadval <109713007+gadvalsreenivasulu@users.noreply.github.com>
2022-10-27 23:51:05 +0530

    Update deploy_dev.sh

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,9 +1,12 @@
 sshpass -p 'Onstak123' ssh -o StrictHostKeyChecking=no user@172.16.180.71 << EOF
-    mkdir Folder
-    cd Folder               
-    mkdir Folder1
-
+ 
     #cd score-ui/
     #git pull
+    
+    echo 'build'
     #npm run build_dev;
+    
+     echo 'deploy in dev'
+     #sudo cp -r ./dist/UI/* /var/www/html;
+
 EOF
