a5743d80f6bf8c94208cc9b0148c874fc13493ce
sgadval <109713007+gadvalsreenivasulu@users.noreply.github.com>
2022-10-27 23:52:23 +0530

    Update deploy_qa.sh

--- deploy_qa.sh
+++ deploy_qa.sh
@@ -3,5 +3,11 @@
 sshpass -p 'Onstak123' ssh -o StrictHostKeyChecking=no user@172.16.186.169 << EOF
                 #cd score-ui
                 #git pull
+                
+                echo 'build'
                 #npm run build_qa;
+                
+                 echo 'deplot in dev'
+                #sudo cp -r ./dist/UI/* /var/www/html;
+
 EOF
